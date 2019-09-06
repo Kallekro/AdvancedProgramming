cc36366115e4cd1cebaeb047605d955d031b3f28
Kalle Kromann <kallekro@gmail.com>
2019-09-06 11:05:13 +0200

    test time

--- a0/code/part2/src/Arithmetic.hs
+++ a0/code/part2/src/Arithmetic.hs
@@ -18,16 +18,19 @@ where
 
 import Definitions
 
-parenthesize :: String -> String
-parenthesize s = "(" ++ s ++ ")"
+-- parenthesize :: String -> String
+-- parenthesize s = "(" ++ s ++ ")"
+
+showOp :: Exp -> Exp -> String -> String
+showOp e1 e2 op = "(" ++ showExp e1 ++ op ++ showExp e2 ++ ")"
 
 showExp :: Exp -> String
 showExp (Cst exp) = show exp
-showExp (Add exp1 exp2) = parenthesize (showExp exp1 ++ " + " ++ showExp exp2)
-showExp (Sub exp1 exp2) = parenthesize (showExp exp1 ++ " - " ++ showExp exp2)
-showExp (Mul exp1 exp2) = parenthesize (showExp exp1 ++ " * " ++ showExp exp2)
-showExp (Div exp1 exp2) = parenthesize (showExp exp1 ++ " / " ++ showExp exp2)
-showExp (Pow exp1 exp2) = parenthesize (showExp exp1 ++ " ^ " ++ showExp exp2)
+showExp (Add exp1 exp2) = showOp exp1 exp2 "+"-- parenthesize (showExp exp1 ++ " + " ++ showExp exp2)
+showExp (Sub exp1 exp2) = showOp exp1 exp2 "-"-- parenthesize (showExp exp1 ++ " - " ++ showExp exp2)
+showExp (Mul exp1 exp2) = showOp exp1 exp2 "*"-- parenthesize (showExp exp1 ++ " * " ++ showExp exp2)
+showExp (Div exp1 exp2) = showOp exp1 exp2 "/"-- parenthesize (showExp exp1 ++ " / " ++ showExp exp2)
+showExp (Pow exp1 exp2) = showOp exp1 exp2 "^"-- parenthesize (showExp exp1 ++ " ^ " ++ showExp exp2)
 showExp _ = error "Expression is not supported by showExp."
 
 evalSimple :: Exp -> Integer
@@ -52,7 +55,7 @@ evalFull (Cst exp) _ = exp
 evalFull (Add exp1 exp2) r = evalFull exp1 r + evalFull exp2 r
 evalFull (Sub exp1 exp2) r = evalFull exp1 r - evalFull exp2 r
 evalFull (Mul exp1 exp2) r = evalFull exp1 r * evalFull exp2 r
-evalFull (Div exp1 exp2) r = 
+evalFull (Div exp1 exp2) r =
   case evalFull exp2 r of 0 -> error "Division by zero."
                           n -> evalFull exp1 r `div` n
 evalFull (Pow exp1 exp2) r =
@@ -62,10 +65,11 @@ evalFull (Pow exp1 exp2) r =
 evalFull (If e1 e2 e3) r =
   case evalFull e1 r of 0 -> evalFull e3 r
                         _ -> evalFull e2 r
-evalFull (Var v) r = 
+evalFull (Var v) r =
   case r v of Just n -> n
               Nothing -> error ("Undefined variable " ++ v)
 evalFull (Let v e1 e2) r = evalFull e2 (extendEnv v (evalFull e1 r) r)
+evalFull (Sum _ _ _ _) _ = 0
 
 evalErr :: Exp -> Env -> Either ArithError Integer
 evalErr = undefined
--- a0/code/part2/tests/Test.hs
+++ a0/code/part2/tests/Test.hs
@@ -5,18 +5,70 @@ import Arithmetic
 
 import Data.List (intercalate)
 import System.Exit (exitSuccess, exitFailure)  -- for when running stand-alone
+import Control.Exception
+
+-- environments for testing extendEnv
+testEnv1 = extendEnv "c" 100 (extendEnv "b" 10 (extendEnv "a" 5 initEnv))
+testEnv2 = extendEnv "d" 25 (extendEnv "b" 99 (extendEnv "c" (-2) testEnv1))
 
 tests :: [(String, Bool)]
-tests = [test1, test2, test3] where
-  test1 = ("test1", evalSimple (Add (Cst 2) (Cst 2)) == 4)
-  test2 = ("test2", evalFull (Let "a" (Cst 42) (Var "a")) initEnv == 42)
-  test3 = ("test3", evalErr (Var "x") initEnv == Left (EBadVar "x"))
+tests = [
+  -- evalSimple tests
+  ("test_ES_0", evalSimple (Cst 13) == 13),
+  ("test_ES_1", evalSimple (Add (Cst 2) (Cst 2)) == 4),
+  ("test_ES_2", evalSimple (Sub (Cst 3) (Cst 5)) == -2),
+  ("test_ES_2", evalSimple (Mul (Cst 5) (Cst 10)) == 50),
+  ("test_ES_3", evalSimple (Div (Cst 15) (Cst 3)) == 5),
+  ("test_ES_4", evalSimple (Pow (Cst 2) (Cst 4)) == 16),
+  ("test_ES_5", evalSimple (Mul (Sub (Add (Cst 100)
+    (Div (Pow (Cst 2) (Cst 4)) (Cst 4))) (Cst 60)) (Cst 2)) == 88),
+
+  -- extendEnv tests
+  ("test_EX_0", testEnv1 "b" == Just 10),
+  ("test_EX_1", testEnv2 "b" == Just 99),
+  ("test_EX_2", testEnv1 "d" == Nothing),
+  ("test_EX_3", testEnv2 "d" == Just 25),
+  ("test_EX_4", testEnv1 "a" == Just 5),
+  ("test_EX_5", testEnv2 "a" == Just 5),
+
+  -- evalFull tests
+  ("test_EF_0", evalFull (Cst 13) initEnv == 13),
+  ("test_EF_1", evalFull (Add (Cst 2) (Cst 2)) initEnv == 4),
+  ("test_EF_2", evalFull (Sub (Cst 3) (Cst 5)) initEnv == -2),
+  ("test_EF_2", evalFull (Mul (Cst 5) (Cst 10)) initEnv == 50),
+  ("test_EF_3", evalFull (Div (Cst 15) (Cst 3)) initEnv == 5),
+  ("test_EF_4", evalFull (Pow (Cst 2) (Cst 4)) initEnv == 16),
+  ("test_EF_5", evalFull (Mul (Sub (Add (Cst 100)
+    (Div (Pow (Cst 2) (Cst 4)) (Cst 4))) (Cst 60)) (Cst 2)) initEnv == 88),
+  ("test_EF_6", evalFull (Add (Var "a") (Var "b")) testEnv1 == 15),
+  ("test_EF_7", evalFull (Mul (Var "d") (Cst 4)) testEnv2 == 100),
+  ("test_EF_8", evalFull (If (Cst 1) (Cst 10) (Cst 20)) initEnv == 10),
+  ("test_EF_9", evalFull (If (Cst 0) (Pow (Cst 10) (Cst (-2))) (Cst 20)) initEnv == 20),
+  ("test_EF_10", evalFull (Var "c") testEnv2 == (-2)),
+  ("test_EF_11", evalFull (Let "a" (Cst 42) (Var "a")) initEnv == 42),
+  ("test_EF_12", evalFull (Let "a" (Div (Cst 2) (Cst 0)) (Cst 10)) initEnv == 10),
+  ("test_EF_13", evalFull (Sum "i" (Cst 1) (Cst 10)
+    (Mul (Var "i") (Cst 2))) initEnv == 110),
+  ("test_EF_14", evalFull (Sum "x" (Cst 1) (Add (Cst 2) (Cst 2))
+    (Mul (Var "x") (Var "x"))) initEnv == 30),
+
+  -- TODO: evalErr tests
+  -- ("test3", evalErr (Var "x") initEnv == Left (EBadVar "x"))
+
+  -- TODO: Maybe test error handling
+  -- ("errTest_EF_0", ((evalFull (Div (Cst 2) (Cst 0)) initEnv) `catch` handler) /= "Division by zero."),
+
+  ("dummy_test", True)]
+
+
+handler :: IOError -> IO ()
+handler e = putStrLn (show e)
 
 main :: IO ()
 main =
   let failed = [name | (name, ok) <- tests, not ok]
   in case failed of
-       [] -> do putStrLn "All tests passed!"
+       [] -> do putStrLn $ "\nAll " ++ (show (length tests)) ++ " tests passed!"
                 exitSuccess
-       _ -> do putStrLn $ "Failed tests: " ++ intercalate ", " failed
+       _ -> do putStrLn $ "\n" ++ (show (length failed)) ++ " failed tests: " ++ intercalate ", " failed
                exitFailure
