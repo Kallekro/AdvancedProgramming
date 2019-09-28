211df70bc4f9aa1a2179c0c45fc3d092424d13d6
Kalle Kromann <kallekro@gmail.com>
2019-09-29 01:58:44 +0200

    some stuff

--- a3/code/part2/instahub.pl
+++ a3/code/part2/instahub.pl
@@ -4,18 +4,110 @@
 %%% level 0 %%%
 
 % follows(G, X, Y)
-    
+% case 1: If the first element of X's follow list is Y, succeed.
+follows([person(X, [Y|_])|_], X, Y).
+% case 2: Recurse with the first element of X's follow list removed
+follows([person(X, [_|Fs])|_], X, Y) :- follows([person(X, Fs)], X, Y).
+% case 3: If the first element of G is not X remove it and recurse
+follows([_|Gs], X, Y) :- follows(Gs, X, Y).
+
+% aux. predicate: concat(L1, L2, L3)
+% for concatting two lists L1 and L2 resulting in L3.
+concat([],L,L).
+concat([H|T], L2, [H|L3]) :- concat(T, L2, L3).
+
+% aux. predicate: different(G, X, Y)
+% for checking if X and Y are two different members of G.
+% case 1+2: If G starts with (X,Y) or (Y,X), succeed.
+different([person(X,_),person(Y,_)|_], X, Y).
+different([person(Y,_),person(X,_)|_], X, Y).
+% case 3+4: If G starts with (X,Z) or (Y,Z) remove Z and recurse.
+different([person(X,XF),_|Gs], X, Y) :- different([person(X,XF)|Gs], X, Y).
+different([person(Y,YF),_|Gs], X, Y) :- different([person(Y,YF)|Gs], X, Y).
+% case 5: Remove the first element of G and recurse.
+different([_|Gs], X, Y) :- different(Gs, X, Y).
+
 % ignores(G, X, Y)
+% case 1: If X has empty follow list and Y follows X, succeed.
+ignores([person(X,[])|Gs], X, Y) :- follows(Gs, Y, X).
+% case 2: If the first element of X's follow list is not Y,
+% recurse by removing that element from X's follow list.
+ignores([person(X,[XF|XFs])|Gs], X, Y) :-
+    different(Gs, Y, XF),
+    ignores([person(X,XFs)|Gs], X, Y).
+% case 3: If the first element of G is not X,
+% re-position it to the back and recurse.
+ignores([person(Z,ZF)|Gs], X, Y) :-
+    different([person(Z,ZF)|Gs], Z, X),
+    concat(Gs, [person(Z,ZF)], Gnew),
+    ignores(Gnew, X, Y).
 
 %%% level 1 %%%
 
 % popular(G, X)
+popular([person(X,[])|_], X).
+popular([person(X, [XF|XFs])|Gs], X) :-
+    follows([person(X,[XF|XFs])|Gs], XF, X),
+    popular([person(X,XFs)|Gs], X).
+popular([person(Z,ZF)|Gs], X) :-
+    different([person(Z,ZF)|Gs], Z, X),
+    concat(Gs, [person(Z,ZF)], Gnew),
+    popular(Gnew, X).
 
 % outcast(G, X)
+outcast([person(X,[])|_], X).
+outcast([person(X, [XF|XFs])|Gs], X) :-
+    ignores([person(X,[XF|XFs])|Gs], XF, X),
+    outcast([person(X,XFs)|Gs], X).
+outcast([person(Z,ZF)|Gs], X) :-
+    different([person(Z,ZF)|Gs], Z, X),
+    concat(Gs, [person(Z,ZF)], Gnew),
+    outcast(Gnew, X).
 
 % friendly(G, X)
+friendly(G, X) :- friendlyHelper(G, G, X).
+
+friendlyHelper(_, [], _).
+friendlyHelper(Gorg, [person(Y, _)|Gs], X) :-
+    different(Gorg, X, Y),
+    follows(Gorg, Y, X),
+    follows(Gorg, X, Y),
+    friendlyHelper(Gorg, Gs, X).
+
+friendlyHelper(Gorg, [person(Z, [])|Gs], X) :-
+    different(Gorg, Z, X),
+    friendlyHelper(Gorg, Gs, X).
+
+friendlyHelper(Gorg, [person(Z, [ZF|ZFs])|Gs], X) :-
+    different(Gorg, Z, X),
+    different(Gorg, ZF, X),
+    friendlyHelper(Gorg, [person(Z, ZFs)|Gs], X).
+
+friendlyHelper(Gorg, [person(X,_),Y|Gs], X) :-
+    friendlyHelper(Gorg, [Y|Gs], X).
 
 % hostile(G, X)
+hostile(G, X) :- hostileHelper(G, G, X).
+
+hostileHelper(_, [], _).
+hostileHelper(Gorg, [person(Y, _)|Gs], X) :-
+    different(Gorg, X, Y),
+    follows(Gorg, Y, X),
+    ignores(Gorg, X, Y),
+    hostileHelper(Gorg, Gs, X).
+
+hostileHelper(Gorg, [person(Z, [])|Gs], X) :-
+    different(Gorg, Z, X),
+    hostileHelper(Gorg, Gs, X).
+
+hostileHelper(Gorg, [person(Z, [ZF|ZFs])|Gs], X) :-
+    different(Gorg, Z, X),
+    different(Gorg, ZF, X),
+    hostileHelper(Gorg, [person(Z, ZFs)|Gs], X).
+
+hostileHelper(Gorg, [person(X,_),Y|Gs], X) :-
+    hostileHelper(Gorg, [Y|Gs], X).
+
 
 %%% level 2 %%%
 
--- a3/code/part2/instatest.pl
+++ a3/code/part2/instatest.pl
@@ -17,6 +17,7 @@ g2([person(batman, [green_arrow, superman]),
 
 :- begin_tests(instahub).
 
+% follows
 test(follows1, [nondet]) :-
     g1(G), follows(G, bruce, clark).
 
@@ -25,5 +26,71 @@ test(follows2, [fail]) :-
 
 test(follows3, [set(X == [barry,clark,oliver])]) :-
     g1(G), follows(G, X, kara).
+
+% different
+test(different1, [nondet]) :-
+    g1(G), different(G, kara, bruce).
+
+test(different2, [fail]) :-
+    g1(G), different(G, kara, kara).
+
+test(different3, [fail]) :-
+    g1(G), different(G, john, kara).
+
+test(different4, [set(X == [bruce, barry, clark, oliver])]) :-
+    g1(G), different(G, X, kara).
+
+% ignores
+test(ignores1, [nondet]) :-
+    g1(G), ignores(G, kara, oliver).
+
+test(ignores2, [fail]) :-
+    g1(G), ignores(G, oliver, kara).
+
+test(ignores3, [fail]) :-
+    g1(G), ignores(G, bruce, barry).
+
+test(ignores4, [set(X == [oliver])]) :-
+    g1(G), ignores(G, kara, X).
+
+% popular
+test(popular1, [nondet]) :-
+    g1(G), popular(G, kara).
+
+test(popular2, [fail]) :-
+    g1(G), popular(G, clark).
+
+test(popular3, [set(X == [kara])]) :-
+    g1(G), popular(G, X).
+
+% outcast
+test(outcast1, [nondet]) :-
+    g1(G), outcast(G, bruce).
+
+test(outcast2, [fail]) :-
+    g1(G), outcast(G, kara).
+
+test(outcast3, [set(X == [bruce, oliver])]) :-
+    g1(G), outcast(G, X).
+
+% friendly
+test(friendly1, [nondet]) :-
+    g1(G), friendly(G, barry).
+
+test(friendly2, [fail]) :-
+    g1(G), friendly(G, kara).
+
+test(friendly3, [set(X == [barry, bruce])]) :-
+    g1(G), friendly(G, X).
+
+% hostile
+test(hostile1, [nondet]) :-
+    g1(G), hostile(G, oliver).
+
+test(hostile2, [fail]) :-
+    g1(G), hostile(G, kara).
+
+test(hostile3, [set(X == [oliver])]) :-
+    g1(G), hostile(G, X).
 
 :- end_tests(instahub).
